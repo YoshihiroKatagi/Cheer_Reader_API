776131206fd4252340e2a901450b3bd6732257b0
YoshihiroKatagi <katagiyoshihiro@gmail.com>
2025-02-10 01:03:26 +0900

    first commit

---
+++ src/main/java/com/katagi/cheer_reader/CheerReaderApplication.java
@@ -0,0 +1,13 @@
+package com.katagi.cheer_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheerReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/katagi/cheer_reader/CheerReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katagi.cheer_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheerReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
